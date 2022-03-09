b9c00bb91c4f38eddb7ab0177a69269982c10222
Cristian Bustamante <cra.bustamante@alumnos.duoc.cl>
2022-03-09 13:00:06 -0300

    cambio de ruta desde script

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,16 +1,35 @@
 <template>
   <h1>Estamos en el juego</h1>
   <p>{{ $route.params.name }}</p>
+  <hr>
+
+
+  <button @click="load">Cargar</button>
 </template>
 
 <script>
-import { useRoute } from "vue-router";
+import { useRoute,useRouter } from "vue-router";
 
 export default {
   setup() {
     const route = useRoute();
+    const router = useRouter();
 
-    console.log(route.params);
+    // console.log(route.params);
+    console.log(router);
+
+    const load = () => {
+       console.log('Ejecutando la petecion');
+
+       setTimeout(() =>{
+           console.log('volver a la home');
+           router.push('/');//redige a la home
+       },3000)
+    }
+
+    return{
+        load,
+    };
   },
 };
 </script>
